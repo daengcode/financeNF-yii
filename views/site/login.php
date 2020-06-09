8710c0ca9a75bf2b11cfba66246bfcc690f83f6c
daengcode <zulkiflijufri58@gmail.com>
2020-06-09 21:15:37 +0700

    Build view login

--- views/site/login.php
+++ views/site/login.php
@@ -11,16 +11,20 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="form-group">
+        <div class="col-sm-offset-5 col-sm-5">
+            <h1><?= Html::encode($this->title) ?></h1>
+            <p>Please fill out the following fields to login:</p>
+        </div>
+    </div>
 
-    <p>Please fill out the following fields to login:</p>
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'layout' => 'horizontal',
         'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
+            'template' => "{label}\n<div class=\"col-sm-3\">{input}</div>\n<div class=\"col-sm-offset-5 col-sm-5\">{error}</div>",
+            'labelOptions' => ['class' => 'col-sm-5 control-label'],
         ],
     ]); ?>
 
@@ -29,11 +33,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= $form->field($model, 'password')->passwordInput() ?>
 
     <?= $form->field($model, 'rememberMe')->checkbox([
-        'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+        'template' => "<div class=\"col-sm-offset-5 col-sm-5\">{input} {label}</div>\n<div class=\"col-sm-8\">{error}</div>",
     ]) ?>
 
     <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
+        <div class="col-sm-offset-5 col-sm-5">
             <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
         </div>
     </div>
