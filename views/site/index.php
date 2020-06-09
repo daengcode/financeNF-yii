83fffcdde0895eb3c899672571d05796e264e86d
daengcode <zulkiflijufri58@gmail.com>
2020-06-09 20:37:40 +0700

    Build Dashboard

---
+++ views/site/index.php
@@ -0,0 +1,28 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Selamat Datang';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h2>Selamat Datang</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, veritatis amet.</p>
+    </div>
+
+    <div class="body-content">
+        <div class="row">
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+
+    </div>
+</div>
