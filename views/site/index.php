76892893b950305dba32c6c2535a463c8b259048
daengcode <zulkiflijufri58@gmail.com>
2020-06-10 20:00:23 +0700

    Update dashboard

--- views/site/index.php
+++ views/site/index.php
@@ -1,28 +1,64 @@
 <?php
 
+use kartik\icons\Icon;
+use yii\bootstrap\Html;
+
 /* @var $this yii\web\View */
 
 $this->title = 'Selamat Datang';
+Icon::map($this);
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h2>Selamat Datang</h2>
+        <?= Icon::show('user-tie', ['class' => 'fa-3x']) ?><h2>Selamat datang, <?= Yii::$app->user->identity->username ?></h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, veritatis amet.</p>
     </div>
 
     <div class="body-content">
         <div class="row">
-            <div class="col-lg-4">
-
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <a href="#" data-toggle="tooltip" title="Registrasi Pembayaran Mahasiswa">
+                            <div class="thumbnail">
+                                <?= Icon::show('money-check-alt', ['class' => 'fa-5x', 'title' => 'Registrasi Pembayaran Mahasiswa']) ?>
+                                <div class="caption">
+                                    <h3>Registrasi Pembayaran</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <a href="#" data-toggle="tooltip" title="Laporan Pembayaran Mahasiswa">
+                            <div class="thumbnail" style="margin-left: 25px;">
+                                <?= Icon::show('file-signature', ['class' => 'title fa-5x', 'title' => 'Laporan Pembayaran Mahasiswa']) ?>
+                                <div class="caption">
+                                    <h3>Laporan Pembayaran</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <a href="#" data-toggle="tooltip" title="Rekap Pembayaran Mahasiswa">
+                            <div class="thumbnail">
+                                <?= Icon::show('file-invoice-dollar', ['class' => 'fa-5x', 'title' => 'Rekap Pembayaran Mahasiswa']) ?>
+                                <div class="caption">
+                                    <h3>Rekap Pembayaran</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
 </div>
